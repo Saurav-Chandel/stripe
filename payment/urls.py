a3fd5cd984a728f8d075f578b6e0ba2c130fcc88
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-13 15:06:36 +0530

    'update_code'

--- payment/urls.py
+++ payment/urls.py
@@ -2,11 +2,23 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+# urlpatterns = [
+#     path('', LandingPage.as_view(), name='landing_page'),
+#     path('webhooks/stripe/', Stripe_webhook, name='stripe-webhook'),
+#     path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
+#     path('cancel/', CancelView.as_view(), name='cancel'),
+#     path('success/', SuccessView.as_view(), name='success'),
+#     path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
+# ]
+
+
 urlpatterns = [
-    path('', LandingPage.as_view(), name='landing_page'),
-    path('webhooks/stripe/', Stripe_webhook, name='stripe-webhook'),
-    path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
-    path('cancel/', CancelView.as_view(), name='cancel'),
-    path('success/', SuccessView.as_view(), name='success'),
-    path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
+    path('', ProductListView.as_view(), name='home'),
+    path('create/', ProductCreateView.as_view(), name='create'),
+    path('detail/<id>/', ProductDetailView.as_view(), name='detail'),
+    path('success/', PaymentSuccessView.as_view(), name='success'),
+    path('failed/', PaymentFailedView.as_view(), name='failed'),
+    path('history/', OrderHistoryListView.as_view(), name='history'),
+
+    path('api/checkout-session/<id>/', create_checkout_session, name='api_checkout_session'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -7,127 +7,233 @@ from payment.models import *
 stripe.api_key = settings.STRIPE_SECRET_KEY
 from django.http import JsonResponse, HttpResponse
 from django.core.mail import send_mail
+from django.views.generic import ListView, CreateView, DetailView, TemplateView
+from django.urls import reverse, reverse_lazy
+from django.http.response import HttpResponseNotFound, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 # Create your views here.
 
-class LandingPage(TemplateView):
-    template_name = "checkout.html"
+# class LandingPage(TemplateView):
+#     template_name = "checkout.html"
+
+#     def get_context_data(self, **kwargs):
+#         product = Product1.objects.get(name="raju")
+#         context = super(LandingPage, self).get_context_data(**kwargs)
+#         context.update({
+#             "product": product,
+#             "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
+#         })
+#         print(context)
+#         return context
+
+
+# class SuccessView(TemplateView):
+#     template_name = "success.html"
+
+
+# class CancelView(TemplateView):
+#     template_name = "cancel.html"
+
+
+# class CreateCheckoutSessionView(View):
+#     def post(self, request, *args, **kwargs):
+#         print('_______________________')
+#         product_id = self.kwargs["pk"]
+#         product = Product1.objects.get(id=product_id)
+#         print(product)
+#         print(product.price)
+#         YOUR_DOMAIN = "http://127.0.0.1:8000"
+#         checkout_session = stripe.checkout.Session.create(
+#             payment_method_types=['card'],
+#             line_items=[
+#                 {
+#                     'price_data': {
+#                         'currency': 'inr',
+#                         'unit_amount': product.price,
+#                         'product_data': {
+#                             'name': product.name,
+#                             'images': ['https://i.imgur.com/EHyR2nP.png'],
+#                         },
+#                     },
+#                     'quantity': 1,
+#                 },
+#             ],
+#             metadata={
+#                 "product_id": product.id
+#             },
+#             mode='payment',
+#             success_url=YOUR_DOMAIN + '/success/',
+#             cancel_url=YOUR_DOMAIN + '/cancel/',
+#         )
+#         return JsonResponse({
+#             'id': checkout_session.id
+#         })
+
+# from django.views.decorators.csrf import csrf_exempt
+# @csrf_exempt
+# def Stripe_webhook(request):
+#   print("+++++++++++")
+#   payload = request.body
+#   sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+#   event = None
+
+#   try:
+#     event = stripe.Webhook.construct_event(
+#       payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+#     )
+#   except ValueError as e:
+#     # Invalid payload
+#     return HttpResponse(status=400)
+#   except stripe.error.SignatureVerificationError as e:
+#     # Invalid signature
+#     return HttpResponse(status=400)
+
+#   # Handle the checkout.session.completed event
+#   if event['type'] == 'checkout.session.completed':
+#     session = event['data']['object']
+#     print(session)
+
+#     customer_email=session['customer_details']['email']
+#     product_id=session['metadata']['product_id']
+
+#     product=Product.objects.get(id=product_id)
+
+#     send_mail(
+#         subject="Here is your Product",
+#         message=f"Thanx for your purchase,Here is the product you ordered {product.url}",
+#         recipient_list=[customer_email],
+#         from_email="chandelsaurav0817@gmail.com"
+#     )
+
+#   # Passed signature verification
+#   return HttpResponse(status=200)
+        
+# import json
+# class StripeIntentView(View):
+#     def post(self,request,*args,**kwargs):
+#         try:
+#             print('________________')
+#             req_json = json.loads(request.body)
+#             print(req_json)
+#             customer = stripe.Customer.create(email=req_json['email'])
+#             print(customer)
+#             product_id = self.kwargs["pk"]
+#             product = Product.objects.get(id=product_id)
+#             intent = stripe.PaymentIntent.create(
+#                 amount=product.price,
+#                 currency='inr',
+#                 customer=customer['id'],
+#                 metadata={
+#                     "product_id": product.id
+#                 }
+#             )
+#             print(intent)
+#             return JsonResponse({
+#                 'clientSecret': intent['client_secret']
+#             })
+#         except Exception as e:
+#             return JsonResponse({ 'error': str(e) })              
+
+
+
+
+
+####################
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+    context_object_name = 'product_list'
+
+ 
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+    template_name = "product_create.html"
+    success_url = reverse_lazy("home")
+
+    
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    pk_url_kwarg = 'id'
+
 
     def get_context_data(self, **kwargs):
-        product = Product.objects.get(name="Book")
-        context = super(LandingPage, self).get_context_data(**kwargs)
-        context.update({
-            "product": product,
-            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
-        })
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        context['stripe_publishable_key'] = settings.STRIPE_PUBLIC_KEY
         print(context)
-        return context
+        return context  
 
-
-class SuccessView(TemplateView):
-    template_name = "success.html"
-
-
-
-class CancelView(TemplateView):
-    template_name = "cancel.html"
-
-
-
-class CreateCheckoutSessionView(View):
-    def post(self, request, *args, **kwargs):
-        print('_______________________')
-        product_id = self.kwargs["pk"]
-        product = Product.objects.get(id=product_id)
-        print(product)
-        print(product.price)
-        YOUR_DOMAIN = "http://127.0.0.1:8000"
-        checkout_session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[
-                {
-                    'price_data': {
-                        'currency': 'inr',
-                        'unit_amount': product.price,
-                        'product_data': {
-                            'name': product.name,
-                            'images': ['https://i.imgur.com/EHyR2nP.png'],
-                        },
-                    },
-                    'quantity': 1,
-                },
-            ],
-            metadata={
-                "product_id": product.id
-            },
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success/',
-            cancel_url=YOUR_DOMAIN + '/cancel/',
-        )
-        return JsonResponse({
-            'id': checkout_session.id
-        })
-
-from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
-def Stripe_webhook(request):
-  print("+++++++++++")
-  payload = request.body
-  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-  event = None
+def create_checkout_session(request, id):
 
-  try:
-    event = stripe.Webhook.construct_event(
-      payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-    )
-  except ValueError as e:
-    # Invalid payload
-    return HttpResponse(status=400)
-  except stripe.error.SignatureVerificationError as e:
-    # Invalid signature
-    return HttpResponse(status=400)
+    request_data = json.loads(request.body)
+    product = get_object_or_404(Product, pk=id)
 
-  # Handle the checkout.session.completed event
-  if event['type'] == 'checkout.session.completed':
-    session = event['data']['object']
-    print(session)
-
-    customer_email=session['customer_details']['email']
-    product_id=session['metadata']['product_id']
-
-    product=Product.objects.get(id=product_id)
-
-    send_mail(
-        subject="Here is your Product",
-        message=f"Thanx for your purchase,Here is the product you ordered {product.url}",
-        recipient_list=[customer_email],
-        from_email="chandelsaurav0817@gmail.com"
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        # Customer Email is optional,
+        # It is not safe to accept email directly from the client side
+        customer_email = request_data['email'],
+        payment_method_types=['card'],
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'inr',
+                    'product_data': {
+                    'name': product.name,
+                    },
+                    'unit_amount': int(product.price * 100),
+                },
+                'quantity': 1,
+            }
+        ],
+        mode='payment',
+        success_url=request.build_absolute_uri(
+            reverse('success')
+        ) + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_absolute_uri(reverse('failed')),
     )
 
-  # Passed signature verification
-  return HttpResponse(status=200)
+    # OrderDetail.objects.create(
+    #     customer_email=email,
+    #     product=product, ......
+    # )
+
+    order = OrderDetail()
+    order.customer_email = request_data['email']
+    order.product = product
+    order.stripe_payment_intent = checkout_session['payment_intent']
+    order.amount = int(product.price * 100)
+    order.save()
+
+    # return JsonResponse({'data': checkout_session})
+    return JsonResponse({'sessionId': checkout_session.id})
+
+class PaymentSuccessView(TemplateView):
+    template_name = "payment_success.html"
+
+    def get(self, request, *args, **kwargs):
+        session_id = request.GET.get('session_id')
+        if session_id is None:
+            return HttpResponseNotFound()
         
-import json
-class StripeIntentView(View):
-    def post(self,request,*args,**kwargs):
-        try:
-            print('________________')
-            req_json = json.loads(request.body)
-            print(req_json)
-            customer = stripe.Customer.create(email=req_json['email'])
-            print(customer)
-            product_id = self.kwargs["pk"]
-            product = Product.objects.get(id=product_id)
-            intent = stripe.PaymentIntent.create(
-                amount=product.price,
-                currency='inr',
-                customer=customer['id'],
-                metadata={
-                    "product_id": product.id
-                }
-            )
-            print(intent)
-            return JsonResponse({
-                'clientSecret': intent['client_secret']
-            })
-        except Exception as e:
-            return JsonResponse({ 'error': str(e) })              
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        session = stripe.checkout.Session.retrieve(session_id)
+
+        order = get_object_or_404(OrderDetail, stripe_payment_intent=session.payment_intent)
+        order.has_paid = True
+        order.save()
+        return render(request, self.template_name)
+
+class PaymentFailedView(TemplateView):
+    template_name = "payment_failed.html"
+
+class OrderHistoryListView(ListView):
+    model = OrderDetail
+    template_name = "order_history.html"
